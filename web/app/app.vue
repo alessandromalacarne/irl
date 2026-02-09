1f985f12c87e5c56b3445df4cea24695078447e5
alsoasnerd <alsoasnerd@gmail.com>
2026-02-08 23:01:23 -0300

    refactor(web): move shortener stub to `composable/shortener.ts`

--- web/app/app.vue
+++ web/app/app.vue
@@ -1,19 +1,21 @@
 <script setup>
-import { ref } from 'vue';
+import shorten from './composable/shortener.ts'
 
 const userUrl = ref('');
 const output = ref('');
 
-const submitUrl = () => {
-  output.value = `Shortening: ${userUrl.value}`;
-}
+const clickShort = () => {
+  const shortenResult = shorten(userUrl.value);
+  output.value = shortenResult;
+};
 
 </script>
+
 <template>
   <div>
     <h1>Shorl</h1>
     <input v-model="userUrl">
-    <button @click="submitUrl">Short it</button>
+    <button @click="clickShort">Short it</button>
     <p>{{ output }}</p>
   </div>
 </template>
