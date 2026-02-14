8d728019a853c211e3dd18556a8213b3db1ec90f
alsoasnerd <alsoasnerd@gmail.com>
2026-02-14 15:27:17 -0300

    refactor: remove useless infra files at `./`

--- bin/shorl.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { IrlStack } from '../lib/irl-stack';
-
-const app = new cdk.App();
-new IrlStack(app, 'IrlStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
