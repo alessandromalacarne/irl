60da0bfe085a9ca13cb7a0e715845c8cffff3476
alsoasnerd <alsoasnerd@gmail.com>
2026-02-17 23:45:55 -0300

    build(web): enable `aws-lambda` preset for nitro (#12)

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'aws-lambda',
+    serverStatic: true
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
