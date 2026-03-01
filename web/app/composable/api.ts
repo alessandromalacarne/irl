71d19b8fc0017955f2c32c3a089fca244fc1c9c0
alsoasnerd <alsoasnerd@gmail.com>
2026-03-01 11:57:55 -0300

    feat(web): use `apiUrl` variable at code (#29)

--- web/app/composable/api.ts
+++ web/app/composable/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import type { SendUrlResponse } from "../../types";
 
 async function sendUrl(url: string): Promise<SendUrlResponse> {
-    const response = await axios.post('http://localhost:3000/api/insert', {
+    const config = useRuntimeConfig()
+    const response = await axios.post(`${config.public.apiUrl}/api/insert`, {
         url
     });
 
